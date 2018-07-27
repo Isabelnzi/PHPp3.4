8c42e026016948c5fc2c35d2323e2e60999c3964
Nzingani <isabelnzingani@outlook.com>
2018-07-27 10:56:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex4</title>
+  </head>
+  <body>
+    <?php
+    $nombre = 1;
+while($nombre < 10)
+{
+echo $nombre;
+$nombre =$nombre + $nombre/2 ;// incrémenter
+}
+  ?>
+</body>
+</html>
